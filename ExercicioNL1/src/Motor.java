8545d8d46b0aa8a1e01c4ea1a67c04ce6014c998
janainavanzeli <127895192+janainavanzeli@users.noreply.github.com>
2024-05-02 18:37:19 -0300

    Delete ExercicioNL1 directory

--- ExercicioNL1/src/Motor.java
+++
@@ -1,9 +0,0 @@
-class Motor {
-    private int potencia;
-    private boolean turbo;
-
-    public Motor(int potencia, boolean turbo) {
-        this.potencia = potencia;
-        this.turbo = turbo;
-    }
-}
