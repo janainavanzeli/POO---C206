7f0ef4a45156139e945b6f77cdbcac5f736445be
janainavanzeli <127895192+janainavanzeli@users.noreply.github.com>
2024-05-02 18:36:24 -0300

    Update Motor.java

--- ExercicioNL1/src/Motor.java
+++ ExercicioNL1/src/Motor.java
@@ -1,21 +1,9 @@
-public class Motor {
+class Motor {
     private int potencia;
     private boolean turbo;
 
     public Motor(int potencia, boolean turbo) {
         this.potencia = potencia;
         this.turbo = turbo;
-    }
-
-    @Override
-    public java.lang.String toString() {
-        return "Motor{" +
-                "Potencia: " + potencia +
-                ", Turbo: " + turbo +
-                '}';
-    }
-
-    public int getPotencia() {
-        return potencia;
     }
 }
